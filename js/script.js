ddfccdae36d75ddbf87e50bf2547ecdf42f608f2
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-06 13:51:28 -0300

    Adiciona script nas páginas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-new SimpleSlide({
-  slide: 'quote',
-  nav: false,
-  auto: true,
-  time: 5000, 
-  pauseOnHover: true,
-})
+if(window.SimpleSlide) {
+  new SimpleSlide({
+    slide: 'quote',
+    time: 5000, 
+  })
+  
+  new SimpleSlide({
+    slide: 'portfolio',
+    time: 5000,
+    nav: true,
+  })
+}
