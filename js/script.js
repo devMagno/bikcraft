8772b2a0b5bece7dc59d2e714979e365c82ed7e8
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-10-07 17:46:02 -0300

    Configura SimpleForm e mensagens de feedback do formulário

--- js/script.js
+++ js/script.js
@@ -13,3 +13,11 @@ if(window.SimpleSlide) {
 if(window.SimpleAnime) {
   new SimpleAnime();
 }
+if(window.SimpleForm){
+  new SimpleForm({
+    form: ".formphp", // seletor do formulário
+    button: "#send", // seletor do botão
+    erro: "<div id='form-error'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente enviar uma mensagem para o nosso email: contato@bikcraft.com.</p></div>", // mensagem de erro
+    sucesso: "<div id='form-success'><h2>Formulário enviado com sucesso!</h2><p>Nossa equipe entrará em contato com você em breve.</p></div>", // mensagem de sucesso
+  });
+}
